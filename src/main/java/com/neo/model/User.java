49a978de288c6dbe0dc69132b591b0fc0322d4a7
huyuhang <18035445828@163.com>
2019-07-09 15:55:48 +0800

    saxsfdsf s

--- src/main/java/com/neo/model/User.java
+++ src/main/java/com/neo/model/User.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private Long id;private Long id;
+	private Long id;private Long ids;
 	private String userName;
 	private String password;
 	private String email;
