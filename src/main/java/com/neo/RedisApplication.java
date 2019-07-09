48823cd3c85c8ad90a59547c144c02df20360841
adminhuyh <35656551+adminhuyh@users.noreply.github.com>
2019-07-09 15:29:36 +0800

    Update RedisApplication.java
    dfdfdfdf

--- src/main/java/com/neo/RedisApplication.java
+++ src/main/java/com/neo/RedisApplication.java
@@ -11,5 +11,7 @@ public class RedisApplication {
 		SpringApplication.run(RedisApplication.class, args);
 		
 		SpringApplication.run(RedisApplication.class, args);
+		SpringApplication.run(RedisApplication.class, args);
+		SpringApplication.run(RedisApplication.class, args);
 	}
 }
