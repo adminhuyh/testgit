78142a5e369a8a0882ed105051fce32640217e87
huyuhang <18035445828@163.com>
2019-07-09 17:01:20 +0800

    nine commit

--- src/main/java/com/neo/config/SessionConfig.java
+++ src/main/java/com/neo/config/SessionConfig.java
@@ -7,4 +7,5 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
 public class SessionConfig {
 	public static final String str="";
+	public static final String str1="";
 }
