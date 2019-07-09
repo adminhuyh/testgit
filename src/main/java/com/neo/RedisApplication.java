b70df31657dd79dc55338d8fda204fe46c4b674c
huyuhang <18035445828@163.com>
2019-07-09 15:25:38 +0800

    commit

--- src/main/java/com/neo/RedisApplication.java
+++ src/main/java/com/neo/RedisApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedisApplication {
 
 	public static void main(String[] args) {
+		
+		
+		
 		SpringApplication.run(RedisApplication.class, args);
 	}
 }
